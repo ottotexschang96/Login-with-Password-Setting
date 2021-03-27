aa7fcdc3b2987c302ecf3d35f8a8ec137386d0b8
Otto Chang <schang96@gatech.edu>
2021-03-26 22:25:43 -0700

    second version

--- 20210326Chapter3LoginwithPasswordSetting.py
+++ 20210326Chapter3LoginwithPasswordSetting.py
@@ -7,5 +7,9 @@ while i < 3:
 		print('Successful login')
 		break
 	elif password != 'a123456':
-		print('Wrong password, and you still have remaining chance(s):', 2-i)
+		print('Wrong password')
+		if 2-i > 0:
+			print('and you still have remaining chance(s):', 2-i)
+		else: 
+			print('Your account has been locked, and please contact the customer service.')
 	i = i + 1
